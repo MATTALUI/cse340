d1aef01b3d81f5e3505885b0715e4bb269f4dff9
Matt Hummer <matt.hummer@nutrien.com>
2021-05-29 20:57:18 -0600

    improve validation

--- phpmotors/library/fileops.php
+++ phpmotors/library/fileops.php
@@ -4,7 +4,9 @@
     // @TODO: validate based on file extension
     // @TODO: create thumbnail from original
     $dir_path = $_SERVER['DOCUMENT_ROOT'].'/phpmotors/assets/images/vehicles/'.$vehicleId;
-    $name = $invImage['name'];
+    $path_parts = pathinfo($invImage['name']);
+    $extension = $path_parts['extension'];
+    $name = 'img.'.$extension;
     $img_path = $dir_path.'/'.$name;
     mkdir($dir_path, 0777, true);
     copy($invImage['tmp_name'], $img_path);
--- phpmotors/vehicles/index.php
+++ phpmotors/vehicles/index.php
@@ -15,16 +15,39 @@
 
 	switch ($action){
 		case 'Create':
+			$invMake = filter_input(INPUT_POST, 'invMake');
+			$invModel = filter_input(INPUT_POST, 'invModel');
+			$classificationId = filter_input(INPUT_POST, 'classificationId');
+			$invDescription = filter_input(INPUT_POST, 'invDescription');
+			$invPrice = filter_input(INPUT_POST, 'invPrice');
+			$invStock = filter_input(INPUT_POST, 'invStock');
+			$invColor = filter_input(INPUT_POST, 'invColor');
+
+			if (
+				empty($invMake) ||
+				empty($invModel) ||
+				empty($classificationId) ||
+				empty($invDescription) ||
+				empty($invPrice)||
+				empty($invStock) ||
+				empty($invColor)
+			) {
+				$message = $message = '<p class="message-error">Please provide all required information.</p>';
+				include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/views/vehicles/new.php';
+				exit;
+			}
+
+
 			$newVehicleId = createVehicle(
-				filter_input(INPUT_POST, 'invMake'), # invMake
-				filter_input(INPUT_POST, 'invModel'), # invModel
-				filter_input(INPUT_POST, 'classificationId'), # classificationId
-				filter_input(INPUT_POST, 'invDescription'), # invDescription
-				filter_input(INPUT_POST, 'invPrice'), # invPrice
-				filter_input(INPUT_POST, 'invStock'), # invStock
-				filter_input(INPUT_POST, 'invColor'), # invColor
+				$invMake,                      # invMake
+				$invModel,                     # invModel
+				$classificationId,             # classificationId
+				$invDescription,               # invDescription
+				$invPrice,                     # invPrice
+				$invStock,                     # invStock
+				$invColor,                     # invColor
 				$DEFAULT_INVENTORY_IMAGE_PATH, # $invImage
-				$DEFAULT_INVENTORY_IMAGE_PATH # $invThumbnail
+				$DEFAULT_INVENTORY_IMAGE_PATH  # $invThumbnail
 			);
 
 			if ($_FILES['invImage']){
@@ -35,6 +58,7 @@
 			$message = '<p class="message-success">Vehicle successfully created.</p>';
 
 			include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/views/vehicles/manage.php';
+			exit;
 		case 'New':
 			include $_SERVER['DOCUMENT_ROOT'].'/phpmotors/views/vehicles/new.php';
 			exit;
